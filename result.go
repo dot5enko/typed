4389dde43d098ea65a0d7687dec5fd23bc30b729
sergio <sergiydotsenko@gmail.com>
2024-10-18 15:54:27 +0200

    add json marshaler for result and unwrap classic methods

--- result.go
+++ result.go
@@ -18,6 +18,11 @@ func (r Result[T]) String() string {
 	return fmt.Sprintf("#result[%s]{has: %v}", reflect.ValueOf(obj).Type().Name(), r.has)
 }
 
+func (r Result[T]) MarshalJSON() ([]byte, error) {
+	obj := new(T)
+	return []byte(fmt.Sprintf("#result[%s]{has: %v}", reflect.ValueOf(obj).Type().Name(), r.has)), nil
+}
+
 func (opt Result[T]) IsOk() bool {
 	return opt.has
 }
@@ -31,6 +36,16 @@ func (opt Result[T]) Unwrap() T {
 	return *opt.val
 }
 
+func (opt Result[T]) UnwrapDefault() T {
+
+	if opt.IsOk() {
+		return opt.Unwrap()
+	} else {
+		var def T
+		return def
+	}
+}
+
 func (opt Result[T]) UnwrapOrDefault(def T) T {
 
 	if opt.IsOk() {
@@ -38,7 +53,6 @@ func (opt Result[T]) UnwrapOrDefault(def T) T {
 	} else {
 		return def
 	}
-
 }
 
 func (opt Result[T]) UnwrapOrPanic(msg string) T {
@@ -51,6 +65,17 @@ func (opt Result[T]) UnwrapOrPanic(msg string) T {
 
 func (opt Result[T]) UnwrapError() error {
 	return opt.err
+}
+
+func (opt Result[T]) UnwrapClassic() (obj T, e error) {
+
+	if opt.IsOk() {
+		obj = opt.Unwrap()
+	} else {
+		e = opt.UnwrapError()
+	}
+
+	return
 }
 
 func (opt Result[T]) Accept(f func(*T)) (nextResult Result[T]) {
